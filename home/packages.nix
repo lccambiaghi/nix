d4daaef2eace0fa4d1ff4f347f677ef1bfad8098
Luca Cambiaghi <luca.cambiaghi@me.com>
2025-12-09 23:41:10 +0100

    simplify

--- home/packages.nix
+++ home/packages.nix
@@ -5,24 +5,9 @@
       # dev tools
       curl
       vim
-      #tmux
       htop
-      #tree
       ripgrep
       gh
-      #zoxide
-
-      # programming languages
-      #mise # node, deno, bun, rust, python, etc.
-
-      # misc
-      #nil
-      #biome
-      #nixfmt-rfc-style
-      #yt-dlp
-      #ffmpeg
-      #ollama
-
       # fonts
       nerd-fonts.fira-code
       nerd-fonts.fira-mono
